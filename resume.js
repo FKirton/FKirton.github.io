bad181685edf67b16e928cde5b78606ba081f5c2
fkirton <fiona.kirton@gmail.com>
2023-03-15 17:58:34 +0000

    Version 1.0

--- resume.js
+++ resume.js
@@ -10,4 +10,44 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     });
+
+
+    const collapsibleTabs = document.querySelectorAll(".collapsible-tab");
+
+    collapsibleTabs.forEach(collapsible => {
+        collapsible.addEventListener("click", (e) => {
+            collapsibleTabs.forEach((tab) => {
+                if (tab === e.target) {
+                    tab.classList.toggle("active");
+                    const content = tab.nextElementSibling;
+                    if (content.style.display === "block") {
+                        content.style.display = "none";
+                    } else {
+                        content.style.display = "block";
+                    }
+                } else {
+                    const content = tab.nextElementSibling;
+                    content.style.display = "none";
+                }
+            });
+        });
+    });
+
+    window.addEventListener("resize", (event) => {
+        const collapsible_elements = document.querySelectorAll(".collapsible-content");
+
+        collapsible_elements.forEach((element) => {
+            if (window.innerWidth >= 800) {
+                // Make sure all the collapsible content elements are visible once the tabs
+                // have been hidden
+                if (window.getComputedStyle(element.previousElementSibling).getPropertyValue("display") === "none") {
+                    element.style.display = "block";
+                }
+            } else {
+                if (!(element.previousElementSibling.classList.contains("active"))) {
+                    element.style.display = "none";
+                }
+            }
+        });
+    });
 });
